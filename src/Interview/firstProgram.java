60ee4310f79c19548245233e546a9cb568944037
srinivas <srinivas.maqa@gmail.com>
2025-04-16 23:03:59 +0530

    American guy commit

--- src/Interview/firstProgram.java
+++ src/Interview/firstProgram.java
@@ -3,7 +3,8 @@ package Interview;
 public class firstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hell World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello");
 	}
 
 }
---
+++ src/Interview/secondProgram.java
@@ -0,0 +1,10 @@
+package Interview;
+
+public class secondProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello,Hi");
+
+	}
+
+}
